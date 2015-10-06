3e12618cfdf2af71484c4237f00f438592714d98
Rifati <rhuseni@roc-dev.com>
2015-10-06 11:51:23 +0200

    H11

---
+++ src/H11/Opdracht3.java
@@ -0,0 +1,28 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+@SuppressWarnings("serial")
+public class Opdracht3 extends Applet {
+		 
+		
+		public void init() {
+		setSize(500, 500);
+		}
+
+		public void paint(Graphics g){
+			int x = 50;
+			int y = 30;
+			int o = 1;
+			for (int i = 1; i < 30; i+=o ){
+				g.drawString(""+i, x, y);
+				x += 20;
+				o ++;
+				
+				
+			}
+			
+			
+		}
+	}
